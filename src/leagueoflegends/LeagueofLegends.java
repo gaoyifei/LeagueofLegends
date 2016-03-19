ea898c4bb4764d57560764960666ba501e525ce2
gaoyifei <gaoyifei.cs@gmail.com>
2016-03-19 13:29:22 -0400

    Merge remote-tracking branch 'origin/siqi' into yifei
    # Conflicts:
    #	build/classes/leagueoflegends/LeagueofLegends.class
    #	src/JDBC/DBConnection.java
    #	src/leagueoflegends/LeagueofLegends.java

--- src/leagueoflegends/LeagueofLegends.java
+++ src/leagueoflegends/LeagueofLegends.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -37,3 +38,54 @@ public class LeagueofLegends {
     }
     
 }
+=======
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leagueoflegends;
+import JDBC.DBConnection;
+import java.io.IOException;
+import java.sql.SQLException;
+import model.Player;
+import model.Hero;
+
+
+/**
+ *
+ * @author GaoYifei
+ */
+public class LeagueofLegends {
+    public DBConnection db = DBConnection.getInstance();
+    
+    public LeagueofLegends() throws SQLException {
+        this.init();
+        Player p = new Player();
+        p = db.getPlayer(1);
+        
+        //testing
+        int testwin = db.getPlayerWins(3);
+        int testlose = db.getPlayerLose(3);
+        String testmap = db.getMostCommonMap(3);
+        String testfequip = db.getFavorEquip(3);
+        String testfroles = db.getFavorRoles(3);
+        int testtotaltime = db.getTotalTime(3);
+        Hero testhero = db.getBestHero(3);
+        String tstbestequip = db.getFavorEquipOfBestHero(1);
+    }
+    
+    public void init(){
+        
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+       System.out.println("Application start");
+       LeagueofLegends game = new LeagueofLegends();
+      
+    }
+    
+}
+>>>>>>> origin/siqi
